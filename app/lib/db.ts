c5a95388bc06f78298c968ec89397457d2e68f51
羊锡贵 <yangxigui@corp.polyv.net>
2024-09-28 10:36:22 +0800

    db

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,20 +1,23 @@
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
 const env = process.env.CUSTOM_ENV || process.env.NODE_ENV || "local";
 
 console.log('env', env);
 
-let connection: mysql.Connection;
+let pool: mysql.Pool;
 export async function getConnection() {
-  if (!connection) {
-    const dbOptions: mysql.ConnectionOptions = {
+  if (!pool) {
+    const dbOptions: mysql.PoolOptions = {
       host: 'localhost', // 数据库主机
       user: 'root', // 数据库用户名
-      database: 'en_learn' // 数据库名称
+      database: 'en_learn', // 数据库名称
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
     }
     if (env === 'production') {
       dbOptions.password = process.env.MYSQL_PASSWORD;
     }
-    connection = await mysql.createConnection(dbOptions);
+    pool = mysql.createPool(dbOptions);
   }
-  return connection;
+  return pool.promise();
 }
